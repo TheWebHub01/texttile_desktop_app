69bab12cd701a6c027e4a5fc276b809e73d04a82
nidhithewebhub01 <nidhithewebhub01@gmail.com>
2024-11-21 14:39:22 +0530

    register_screen,homescreen

--- lib/constants/texts/text.dart
+++ lib/constants/texts/text.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-Text customText({String? text, TextStyle? style}) => Text(
+Text customText({String? text, TextStyle? style, TextAlign? textAlign}) => Text(
       text!,
       style: style,
+      textAlign: textAlign,
     );
---
+++ lib/logout/logout.dart
@@ -0,0 +1,11 @@
+// logout() async {
+//   SharedPreferences prefs = await SharedPreferences.getInstance();
+//   await prefs.remove('user_email');
+//   await prefs.remove('user_id');
+
+//   // Navigate back to the login screen
+//   Navigator.pushReplacement(
+//     context,
+//     MaterialPageRoute(builder: (context) => const RegisterScreen()),
+//   );
+// }
